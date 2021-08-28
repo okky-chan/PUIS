23e9b6f9caf0f488ad0c89d429c2db2c9e30db46
Okky Dwi Triferyawan <61794853+okky-chan@users.noreply.github.com>
2021-08-28 09:29:42 +0700

    Add files via upload

---
+++ puis.py
@@ -0,0 +1,64 @@
+import time
+from selenium import webdriver
+
+web = webdriver.Chrome()
+web.get('https://puis.president.ac.id')
+
+username = web.find_element_by_id("email")
+password = web.find_element_by_id("pass")
+
+username.send_keys("username")
+password.send_keys("your password")
+
+web.find_element_by_xpath('//*[@id="login"]/dl/dd[3]/input').click()
+time.sleep(2)
+web.get('https://puis.president.ac.id/student/custom_lecturing_evaluation/middle/2002256425/8206')
+
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[3]/td[5]/input')
+RadioButtonPeriod.click() #1
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[4]/td[5]/input')
+RadioButtonPeriod.click() #2
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[5]/td[5]/input')
+RadioButtonPeriod.click() #3
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[6]/td[5]/input')
+RadioButtonPeriod.click() #4
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[7]/td[5]/input')
+RadioButtonPeriod.click() #5
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[8]/td[5]/input')
+RadioButtonPeriod.click() #6
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[9]/td[5]/input')
+RadioButtonPeriod.click() #7
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[10]/td[5]/input')
+RadioButtonPeriod.click() #8
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[11]/td[5]/input')
+RadioButtonPeriod.click() #9
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[12]/td[5]/input')
+RadioButtonPeriod.click() #10
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[13]/td[5]/input')
+RadioButtonPeriod.click() #11
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[14]/td[5]/input')
+RadioButtonPeriod.click() #12
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[15]/td[5]/input')
+RadioButtonPeriod.click() #13
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[16]/td[5]/input')
+RadioButtonPeriod.click() #14
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[17]/td[5]/input')
+RadioButtonPeriod.click() #15
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[18]/td[5]/input')
+RadioButtonPeriod.click() #16
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[19]/td[5]/input')
+RadioButtonPeriod.click() #17
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[20]/td[5]/input')
+RadioButtonPeriod.click() #18
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[21]/td[5]/input')
+RadioButtonPeriod.click() #19
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[22]/td[5]/input')
+RadioButtonPeriod.click() #20
+RadioButtonPeriod = web.find_element_by_xpath('//*[@id="2002256425"]/tbody/tr[23]/td[5]/input')
+RadioButtonPeriod.click() #21
+
+Comment = "The lecturing is good enough"
+com = web.find_element_by_xpath('//*[@id="comment"]')
+com.send_keys(Comment)
+
+web.find_element_by_xpath('//*[@id="2002256425"]/button').click()
